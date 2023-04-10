9cacddd6be2a163e2cd1c98683a31f477d9a28c7
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2023-04-10 18:13:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import streamlit as st
-
-from rate import generate_interval, play_back, play_freq,results1
 results = results1
 ratings = []
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+f = 100
 def main():
     
     st.title("Rate Results")
     state = 0
+    
     while state < len(results):
         st.write(f"Melody: {results[state]}")
-        if st.button("Play", key=f"play{state}"):
-            play_back(play_freq(results)[state])
+        st.audio(note_la+f*state, sample_rate=sample_rate)
+        #play_freq(results)[state])
         rating = st.text_input("Rate this result good(g)/bad(b): ", key=f"input{state}")
         if rating.lower() in [""]:
             pass
@@ -24,4 +30,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-    
