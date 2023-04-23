48c685678ff7e4048c7a7f3fa8e7f6034e63975a
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2023-04-23 22:14:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import streamlit as st
-
+import numpy as np
 from rate import play_freq,generate_interval,results1
 from synth import st_play_back
 results = results1
 ratings = []
 
 def main():
-    
     st.title("Rate Results")
     state = 0
-    
     while state < len(results):
-        st.write(f"Melody: {results[state]}")
+        st.write(f"Melodic_interval: {results[state]}")
         st_play_back(play_freq(results)[state])
-        rating = st.radio("Rate this melody:", ["Good","-", "Bad"], index=1, key=f"radio{state}")
-        if rating != "":
-            if rating == "Good":
-                ratings.insert(state, "g")
-            elif rating == "Bad":
-                ratings.insert(state, "b")
-            else:
-                ratings.insert(state, "")
-        """______________________"""
+        rating = st.radio("Rate this result:", ["Good","-", "Bad"],index=1, key=f"radio{state}")
+        if rating == "Good":
+            ratings.insert(state, "g")
+        elif rating == "Bad":
+            ratings.insert(state, "b")
+        else:
+            ratings.insert(state, "")
         state += 1
     st.success(f"Ratings: {ratings}, {results}")   
     
